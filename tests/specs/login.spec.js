691ab9b3f99e19c67e29a0154d2d02064ee6c291
mathews <mathewsxavier12320@gmail.com>
2024-06-18 19:15:36 +0530

    Initial commityyyuu

--- tests/specs/login.spec.js
+++ tests/specs/login.spec.js
@@ -7,7 +7,7 @@ test('Login to Parabank', async ({ page }) => {
   const homePage = new HomePage(page);
 
   await loginPage.navigate();
-  await loginPage.login('johnnpiinw', 'okpsso[ssp[l'); // Use valid credentials
+  await loginPage.login('johnnpeiinw', 'okpsso[ssp[l'); // Use valid credentials
 
   await homePage.verifyLoginSuccess();
 });
